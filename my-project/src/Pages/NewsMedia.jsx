dd56ed3785644a28ed13847bf4867535a70e0302
Hazzan <taiwoayodele180@gmail.com>
2025-02-17 08:56:06 +0100

    new commit

--- my-project/src/Pages/NewsMedia.jsx
+++ my-project/src/Pages/NewsMedia.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from '../Component/UI/Banner/Banner';
 
 const NewsMedia = () => {
   return (
     <div>
-      
+      {/*<Banner contest={"About Us"} bannerImg={companyBanner} /> */}
     </div>
   )
 }
